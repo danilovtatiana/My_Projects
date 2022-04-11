dc92abaaa0844da0e1e55b7c99a45cde45b7f950
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-04-11 17:27:21 +0300

    display message function

--- Simple Tasker/main.js
+++ Simple Tasker/main.js
@@ -1,18 +1,29 @@
 const submitButton = document.getElementById("submit_button");
 const tasks = document.getElementById('tasks');
+const messageElement = document.getElementById('message');
 submitButton.addEventListener('click', addTask);
 
+function displayMessage(message){
+  messageElement.innerText = message;
+  messageElement.style.visibility = "visible";
+}
+
+displayMessage("Good, you have no tasks today!");
 
 function addTask(){
   const newTask = document.getElementById('newTask');
   if (inputIsValid(newTask.value)) {
     tasks.innerHTML += '<li class = "list-group">' + newTask.value + '</li>';
     newTask.value = "";
+    messageElement.style.visibility = "hidden";
+  }else {
+    displayMessage("Please provide non empty input!")
   }
 
-function inputIsValid(input) {
-  if (input) {
+function inputIsValid(input){
+  if(input){
     return true;
   }
-    return false;
-  }
+  return false;
+}
+}
