7edb880ddbfc12146482d475091bc3e0c5341cd5
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-04-17 19:02:01 +0300

    functional calculator

---
+++ Calculator/calculator_grid.js
@@ -0,0 +1,40 @@
+let display = document.getElementById('display');
+let buttons = Array.from(document.getElementsByClassName('buttons'));
+
+
+buttons.map(button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText){
+            case 'AC':
+                display.innerText = '';
+                break;
+            case '=':
+                try{
+                    display.innerText = eval(display.innerText);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '+/-':
+                if (display.innerText == display.innerText){
+                   display.innerText = - display.innerText;
+                }
+                break;
+            case '%':
+                try{
+                    display.innerText = eval(display.innerText / 100);
+                } catch {
+                    display.innerText = "Error"
+                }
+                break;
+            case '←':
+                if (display.innerText){
+                   display.innerText = display.innerText.slice(0, -1);
+                }
+                break;
+
+            default:
+                display.innerText += e.target.innerText;
+        }
+    });
+});
