a2985116991ded3fa434913229bc06767b8fce11
Danilov Tatiana <67468933+CeaicaTatiana@users.noreply.github.com>
2022-07-07 10:17:46 +0300

    smooth scrolling on click <a>

--- Curtains Shop Website/main.js
+++ Curtains Shop Website/main.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById("myBtn");
 
 window.onload = (event) => {
   // loadArticles(articles);
-  loadMenuItems(event);
+  // loadMenuItems(event);
   loadPortfolio(portfolio);
   loadMap(maps);
   // loadSlides(slides);
@@ -29,6 +29,54 @@ function topFunction() {
   document.documentElement.scrollTop = 0;
 }
 
+
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+
+function elmYPosition(eID) {
+    var elm = document.getElementById(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
+    } return y;
+}
+
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
+  return false;
+}
 //SHOWSLIDE-CONTAINER
 
 // const loadSlides = (list) => {
@@ -71,15 +119,15 @@ function showSlides(n) {
 }
 
 
-const loadMenuItems = (event) => {
-  console.log('event ===> ', event)
-  const parent = document.getElementById('navigation-menu');
-  const menuArray = [];
-  navMenulist.forEach(item => {
-    menuArray.push(`<li class="nav__item"><a href="${item.link}" id="nav_element" class="nav__link">${item.title}</a></li>`);
-  });
-  parent.innerHTML = menuArray.join('');
-}
+// const loadMenuItems = (event) => {
+//   console.log('event ===> ', event)
+//   const parent = document.getElementById('navigation-menu');
+//   const menuArray = [];
+//   navMenulist.forEach(item => {
+//     menuArray.push(`<li class="nav__item"><a href="${item.link}" id="nav_element" class="nav__link">${item.title}</a></li>`);
+//   });
+//   parent.innerHTML = menuArray.join('');
+// }
 
 // const loadArticles = (list) => {
 //     const articleArray = [];
@@ -155,12 +203,12 @@ span.onclick = function() {
 
 /// mock objects 
 
-const navMenulist = [
-  {title: 'Home',link: 'home', id: 'home'},
-  {title: 'Portofoliu', link: 'portfolio', id: 'portfolio'},
-  {title: 'Magazine', link: 'stores', id: 'stores'},
-  {title: 'Contact', link: 'contact', id: 'contact'},
-]
+// const navMenulist = [
+//   {title: 'Home',link: 'home', id: 'home'},
+//   {title: 'Portofoliu', link: 'portfolio', id: 'portfolio'},
+//   {title: 'Magazine', link: 'stores', id: 'stores'},
+//   {title: 'Contact', link: 'contact', id: 'contact'},
+// ]
 
 // const articles = [
 //   {
